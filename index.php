fe946f695126ca913e0ece4a353832512a7c0b1e
imrsic-dev <54667618+imrsic-dev@users.noreply.github.com>
2020-06-15 22:37:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mljet</title>
     <!-- video player VIDEO js -->
     <link href="https://vjs.zencdn.net/7.8.2/video-js.css" rel="stylesheet"/>
